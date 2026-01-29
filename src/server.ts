9b3d8bd5b24f27bd7ea955f04e80e4cd76a8c310
Sneara0 <snearaparvin.cse@gmail.com>
2026-01-30 00:13:57 +0600

    add public routes

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import app from './app';
 import customerRoutes from "./routes/customer.routes";
 import sellerRoutes from "./routes/seller.routes";
 import adminRoutes from "./routes/admin.routes";
+import publicRoutes from "./routes/public.routes";
 const PORT = process.env.PORT || 5000;
 app.use("/api/customer", customerRoutes);
 app.use("/api/seller", sellerRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api", publicRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
