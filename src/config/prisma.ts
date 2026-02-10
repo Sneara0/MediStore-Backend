2e1bde05cc4c0468e4a6a9a79a41003744818188
Sneara0 <snearaparvin.cse@gmail.com>
2026-02-10 23:54:55 +0600

    add

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,12 +1,10 @@
 import "dotenv/config";
-import pg from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from "@prisma/client"; // কাস্টম পাথ বাদ দিয়ে এটি দিন
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "../../generated/prisma";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`
 
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
-export { prisma };
+export { prisma }
